7051b3397016efb2f0b7d78a8862d56ad7bb74a5
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2019-10-16 15:41:54 +0100

    Groundwork
    Started new game idea, included new movement system

---
+++ New Unity Project/Assets/Scripts/MovmentScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentScript : MonoBehaviour
+{
+    float rotSpeedStart = 0;
+    float rotSpeed = 10;
+    float moveSpeedStart = 0;
+    float moveSpeed = 0.5f;
+
+    float acceleration = 0.000000001f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationF = Input.GetAxis("Vertical") * moveSpeed;
+        float translationR = Input.GetAxis("Horizontal") * rotSpeed;
+
+        if(Input.GetKey("left") && (rotSpeedStart < rotSpeed))
+        {
+            rotSpeedStart = rotSpeedStart - acceleration * Time.deltaTime;
+        }
+        else if (Input.GetKey("right") && (rotSpeedStart < rotSpeed))
+        {
+            rotSpeedStart = rotSpeedStart - acceleration * Time.deltaTime;
+        }
+
+        else if (Input.GetKey("up") && (moveSpeedStart < moveSpeed))
+        {
+            moveSpeedStart = moveSpeedStart - acceleration * Time.deltaTime;
+        }
+
+        transform.Translate(translationF, 0, 0);
+
+        transform.Rotate(0, translationR, 0);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrap : MonoBehaviour
+{
+
+    Renderer[] renderers;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool checkRenderers()
+    {
+        foreach(var renderer in renderers)
+        {
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
