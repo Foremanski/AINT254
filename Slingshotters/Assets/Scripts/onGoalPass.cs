c8d53a0b7da9d9f5c7265606a108a0890a8755c7
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2019-11-28 16:00:15 +0000

    Added Goal
    Added a finish goal that sends player to next scene (currently main menu)
    
    Started adding forward particle system

---
+++ Slingshotters/Assets/Scripts/onGoalPass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onGoalPass : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider Player)
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+}
